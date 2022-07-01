fa1832b46b0003ab0fad3fe706a6edaa066ff5fd
Tiago Fonseca <tiago.fons@hotmail.com>
2022-07-01 20:03:36 +0100

    Terminando styled

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,8 +8,8 @@ import { theme } from "../constants/theme";
 
 
 const HeaderContainer = styled.div`
-border: 1px solid lightgrey;
-background-color: #450059;
+/* border: 1px solid lightgrey; */
+background-color: white;
 display: flex;
 /* position: fixed; */
 top:0;
@@ -18,17 +18,21 @@ justify-content: space-between;
 align-items: center;
 padding: 10px 50px 10px 100px;
 z-index: 2;
+max-width: 95%;
+margin: 0 auto;
 
 `
 const Img = styled.img`
     width: 250px;
     padding: 0 0 0 30px;
+    margin-left: 20px;
 
 `
 const BotaoComMargem = styled(Button)`
     margin-right: 40px;
     color: white;
     display: flex;
+    gap: 5px;
     border: 2px solid white;
     /* justify-content: center; */
     /* width: 200px; */
@@ -41,7 +45,7 @@ export default class Header extends React.Component {
         <ChakraProvider theme={theme}>
           <HeaderContainer>
           <Img onClick = {() => this.props.handleHomePage()}src={LogoNinja}/>
-            <BotaoComMargem bg="brand.white" onClick = {() => this.props.handleCartPage()}>Área de Compras<AiOutlineShoppingCart/> </BotaoComMargem>
+            <BotaoComMargem bg="brand.violet" onClick = {() => this.props.handleCartPage()}>Área de Compras <AiOutlineShoppingCart/></BotaoComMargem>
           </HeaderContainer>
         </ChakraProvider>
       </div>
